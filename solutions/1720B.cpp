158c209cb44426418e3deeeb0d6f1062a8e04825
theRunCom <49315766+theRunCom@users.noreply.github.com>
2022-08-31 14:25:59 +0800

    Update 1720B.cpp

--- solutions/1720B.cpp
+++ solutions/1720B.cpp
@@ -1,1 +1,37 @@
+/*
+4
+8
+1 2 2 3 1 5 6 1
+5
+1 2 3 100 200
+4
+3 3 3 3
+6
+7 8 3 1 1 8
+
+9
+297
+0
+14
+
+
+max(a1,a2,…,al−1,ar+1,ar+2,…,an)−min(a1,a2,…,al−1,ar+1,ar+2,…,an)+max(al,…,ar)−min(al,…,ar).
+*/
 #include <iostream>
+#include <algorithm>
+using namespace std;
+const int N = 1e5 + 10;
+int n, m;
+int a[N], s[N];
+int main() {
+    scanf("%d", &n);
+    while (n--) {
+        int sum;
+        scanf("%d", &m);
+        for (int i = 1; i <= m; i++)
+            scanf("%d", &a[i]);
+        for (int i = 1; i <= m; i++)
+            s[i] = s[i - 1] + a[i];
+    }
+    return 0;
+}

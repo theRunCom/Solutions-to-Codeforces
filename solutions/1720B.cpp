9982f75214a4166d30bd5685fe990aee836ece09
theRunCom <49315766+theRunCom@users.noreply.github.com>
2023-05-06 15:14:45 +0800

    👉1720B.Interesting Sum

--- solutions/1720B.cpp
+++ solutions/1720B.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
-const int N = 1e5 + 10;
 int n, m;
-int a[N], s[N];
+vector<int> vec;
 int main() {
     scanf("%d", &n);
     while (n--) {
         int sum;
         scanf("%d", &m);
-        for (int i = 1; i <= m; i++)
-            scanf("%d", &a[i]);
-        sort(a, a + m);
-        cout << a[m - 1] + a[m - 2] - a[0] - a[1] << endl;
+        vec.resize(m);
+        for (auto& c : vec) cin >> c;
+        sort(vec.begin(), vec.end());
+        cout << vec[m - 1] + vec[m - 2] - vec[0] - vec[1] << endl;
     }
     return 0;
 }

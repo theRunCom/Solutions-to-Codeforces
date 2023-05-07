6bbc0949869a70f67134cd3f116a004f00e43f62
theRunCom <1772414249@qq.com>
2023-05-07 14:31:22 +0800

    👉1716A.2-3 Moves

--- solutions/1716A.cpp
+++ solutions/1716A.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main() {
     int n, m;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+    while (n--) {
         scanf("%d", &m);
-        if (m == 1) cout << 2 << endl;
-        else if (m <= 4) cout << m - 3 + 2 << endl;
-        else cout << m - 3 - 2 << endl;
+        if (m == 1)
+            cout << 2 << endl;
+        else
+            cout << (int)ceil(m * 1.0 / 2) << endl;
     }
     return 0;
 }
